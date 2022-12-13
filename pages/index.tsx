0a81bd02999d048c84ac2761fa6fb5389b9045b7
Peter Fritz <me@peterfritz.dev>
2022-12-13 03:16:56 -0300

    feat: add preferences

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,79 @@
 import {
   ActionIcon,
-  Center, Checkbox, CopyButton, Group, Input, NumberInput, Slider, Space, Stack, TextInput,
+  Button,
+  Center, Checkbox, CopyButton, Group, Input, NumberInput, Slider, Space, Stack,
+  TextInput,
 } from '@mantine/core';
+import { useCounter, useId, useLocalStorage } from '@mantine/hooks';
 import { useEffect, useState } from 'react';
 import { FaCopy, FaRegCopy } from 'react-icons/fa';
 import generatePassword, { characterSets, getCharacterSet } from '../utils/password';
 
-const Home = () => {
-  const [length, setLength] = useState(16);
-  const [endLength, setEndLength] = useState(length);
-  const [password, setPassword] = useState('');
-  const [characters, setCharacters] = useState<(keyof typeof characterSets)[]>([
+interface Preferences {
+  savePreferences: boolean,
+  length: number,
+  characters: (keyof typeof characterSets)[],
+}
+
+const DEFAULT_PREFERENCES: Preferences = {
+  savePreferences: true,
+  length: 16,
+  characters: [
     'uppercase',
     'lowercase',
     'numbers',
     'symbols',
-  ]);
+  ],
+};
+
+const Home = () => {
+  const [preferences, setPreferences] = useLocalStorage<Preferences>({
+    key: 'preferences',
+    defaultValue: DEFAULT_PREFERENCES,
+    serialize: (value) => (
+      JSON.stringify(value)
+    ),
+    deserialize: (localStorageValue) => (
+      JSON.parse(localStorageValue)
+    ),
+    getInitialValueInEffect: false,
+  });
+
+  const [savePreferences, setSavePreferences] = useState(preferences.savePreferences);
+  const [length, setLength] = useState(preferences.length);
+  const [endLength, setEndLength] = useState(preferences.length);
+  const [password, setPassword] = useState('');
+  const [characters, setCharacters] = useState(preferences.characters);
+
+  const lengthInput = useId();
+  const [counter, handlers] = useCounter(0);
+
+  const handleSavePreferencesChange = (value: boolean) => {
+    setSavePreferences(value);
+
+    setPreferences((prevState) => ({
+      ...(value ? prevState : DEFAULT_PREFERENCES),
+      savePreferences: value,
+    }));
+  };
 
   useEffect(() => {
+    if (!endLength) {
+      return;
+    }
+
     const newPassword = generatePassword(endLength, getCharacterSet(characters));
 
     setPassword(newPassword);
-  }, [endLength, characters]);
+
+    if (savePreferences) {
+      setPreferences((prevState) => ({
+        ...prevState,
+        length: endLength,
+        characters,
+      }));
+    }
+  }, [savePreferences, endLength, characters, counter]);
 
   return (
     <Center
@@ -30,8 +82,10 @@ const Home = () => {
     >
       <Stack>
         <TextInput
+          label="Password"
+          labelProps={{ size: 'sm' }}
           value={password}
-          size="md"
+          size="sm"
           rightSection={(
             <CopyButton value={password}>
               {({ copied, copy }) => (
@@ -49,7 +103,7 @@ const Home = () => {
         <Checkbox.Group
           label="Character set"
           value={characters}
-          onChange={(val: typeof characters) => setCharacters(val)}
+          onChange={(val: Preferences['characters']) => setCharacters(val)}
           withAsterisk
         >
           <Checkbox value="uppercase" label="Uppercase" />
@@ -57,10 +111,12 @@ const Home = () => {
           <Checkbox value="numbers" label="Numbers" />
           <Checkbox value="symbols" label="Symbols" />
         </Checkbox.Group>
-        <Input.Wrapper size="sm">
-          <Input.Label required>
-            Password length
-          </Input.Label>
+        <Input.Wrapper
+          label="Password length"
+          required
+          size="sm"
+          id={lengthInput}
+        >
           <Space h="xs" />
           <Group grow position="apart">
             <NumberInput
@@ -69,6 +125,7 @@ const Home = () => {
                 setLength(val);
                 setEndLength(val);
               }}
+              id={lengthInput}
               autoComplete="off"
               hideControls
               min={8}
@@ -92,6 +149,23 @@ const Home = () => {
             />
           </Group>
         </Input.Wrapper>
+        <Space h="sm" />
+        <Group position="apart">
+          <Checkbox
+            label="Save preferences"
+            checked={savePreferences}
+            onChange={({ currentTarget: { checked } }) => {
+              handleSavePreferencesChange(checked);
+            }}
+          />
+          <Button
+            onClick={() => {
+              handlers.increment();
+            }}
+          >
+            Generate
+          </Button>
+        </Group>
       </Stack>
     </Center>
   );
